05fbc7cd1288f00b2583eecd2af63fee97d66fbb
jeanmarc77 <jeanmarc.louviaux@gmail.com>
2021-05-17 20:36:20 +0200

    Add files via upload

---
+++ scripts/links.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * /srv/http/123solar/scripts/links.php
+ *
+ * @package default
+ */
+
+
+$JSjquery = "https://code.jquery.com/jquery-3.4.1.min.js' integrity='sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=' crossorigin='anonymous";
+$JSjqui   = "https://code.jquery.com/ui/1.12.1/jquery-ui.min.js' integrity='sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=' crossorigin='anonymous";
+$JSjquit  = "https://code.jquery.com/ui/1.12.1/themes/south-street/jquery-ui.css";
+$HC       = "https://code.highcharts.com/highcharts.js";
+$HCmore   = "https://code.highcharts.com/highcharts-more.js";
+$HCdd     = "https://code.highcharts.com/modules/drilldown.js";
+$HCexp    = "https://code.highcharts.com/modules/exporting.js";
+$HCann   = "https://code.highcharts.com/modules/annotations.js";
+?>
