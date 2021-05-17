501526f12051258670a51684548a072533e547e1
jeanmarc77 <jeanmarc.louviaux@gmail.com>
2021-05-17 19:51:59 +0200

    Add files via upload

---
+++ styles/footer.php
@@ -0,0 +1,11 @@
+          <!-- #EndEditable -->
+          </td>
+          </tr>
+<tr valign="top">
+<td></td>
+<td align='center'><font size="-4"><a href="http://www.123solar.org">Powered by 123Solar</a></font></td>
+<td align='right'></td>
+</tr>
+</table>
+</body>
+</html>
