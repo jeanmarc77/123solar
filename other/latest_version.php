0c2c41602eb96d6d7190fcf1a9d23aba13c7e20c
Jean-Marc Louviaux <jeanmarc.louviaux@gmail.com>
2022-01-03 18:19:25 +0100

    test

--- other/latest_version.php
+++
@@ -1,6 +0,0 @@
-<?php header('Access-Control-Allow-Origin: *'); ?>
-{
-    "LASTVERSION": "123Solar 1.8.4.3",
-    "LINK": "https://123solar.org/downloads/123solar/123solar1.8.4.3.tar.gz",
-    "md5": "cf1c3886fad55f6ef314a9e720a06bc8"
-}
