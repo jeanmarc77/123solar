dc7aaf2dd925eca74494e42e298b337d222215f4
Jean-Marc Louviaux <jeanmarc.louviaux@gmail.com>
2022-03-13 20:02:12 +0100

    links

--- styles/bluepanel/footer.php
+++ styles/bluepanel/footer.php
@@ -12,7 +12,7 @@
     <td width="128">
     </td>
     <td COLSPAN=2> 
-      <p align="center"><br><font size="-4"><a href="http://www.123solar.org">Powered by 123solar</a></font></p>
+      <p align="center"><br><font size="-4"><a href="https://github.com/jeanmarc77/123solar">Powered by 123solar</a></font></p>
     </td>
   </tr>
 </table>
--- styles/jeanmarc/footer.php
+++ styles/jeanmarc/footer.php
@@ -3,7 +3,7 @@
           </tr>
 <tr valign="top">
 <td></td>
-<td align='center'><font size="-4"><a href="http://www.123solar.org">Powered by 123Solar</a></font></td>
+<td align='center'><font size="-4"><a href="https://github.com/jeanmarc77/123solar">Powered by 123Solar</a></font></td>
 <td align='right'></td>
 </tr>
 </table>
