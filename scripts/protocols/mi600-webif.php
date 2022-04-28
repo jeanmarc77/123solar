e1e09f8cf7764a7ef08a272c5b958a8a45c9988a
dr-ni <uwe_niethammer@web.de>
2022-04-28 22:48:02 +0200

    code cleanup

--- scripts/protocols/mi600-webif.php
+++ scripts/protocols/mi600-webif.php
@@ -4,68 +4,69 @@
 if (!defined('checkaccess')) {die('Direct access not permitted');}
 
 $CMD_RETURN = ''; // Always initialize
-$match = '';
-$err = "no error";
+$MATCHES = '';
+$ERR = "0";
 $SDTE = date("Ymd H:i:s");
-$options = ${'COMOPTION'.$invt_num};
-list ($host, $username, $password) = explode(" ", $options,3);
-$url = "http://".$host."/status.html";
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_USERPWD, $username. ":".$password); 
-curl_setopt($ch, CURLOPT_URL, $url );
-curl_setopt($ch, CURLOPT_TIMEOUT, 15);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$CMD_RETURN = (curl_exec($ch));
+$OPTIONS = ${'COMOPTION'.$invt_num};
+list ($HOST, $USER, $PASSWD) = explode(" ", $OPTIONS, 3);
+$URL = "http://".$HOST."/status.html";
+
+// connect to mi600
+$CURL_HANDLE = curl_init();
+curl_setopt($CURL_HANDLE, CURLOPT_USERPWD, $USER. ":".$PASSWD); 
+curl_setopt($CURL_HANDLE, CURLOPT_URL, $URL );
+curl_setopt($CURL_HANDLE, CURLOPT_TIMEOUT, 15);
+curl_setopt($CURL_HANDLE, CURLOPT_RETURNTRANSFER, 1);
+$CMD_RETURN = (curl_exec($CURL_HANDLE));
 // Checking if any error occured during request
-if(curl_error($ch)) {
+if(curl_error($CURL_HANDLE)) {
     $CMD_RETURN = '';
-    $dt = 0;
-    $err=curl_error($ch);
+    $DT = 0;
+    $ERR=curl_error($CURL_HANDLE);
 } else {
     // Decoding data
-    if (preg_match("/webdata_now_p = .*/", $CMD_RETURN, $match)) {
-        $Pnow = (float)preg_replace('/[^0-9.]+/', '', $match[0]); // keep only value
+    if (preg_match("/webdata_now_p = .*/", $CMD_RETURN, $MATCHES)) {
+        $Pnow = (float)preg_replace('/[^0-9.]+/', '', $MATCHES[0]); // keep only value
     } else {
-        $err="parse error";
+        $ERR="parse error";
     }
-    if (preg_match("/webdata_total_e = .*/", $CMD_RETURN, $match)) {
+    if (preg_match("/webdata_total_e = .*/", $CMD_RETURN, $MATCHES)) {
         if (!$Etotal) {
-            $Etotal=(float)preg_replace('/[^0-9.]+/', '', $match[0]); // keep only value
+            $Etotal=(float)preg_replace('/[^0-9.]+/', '', $MATCHES[0]); // keep only value
         } else {
             if($Pnow>0) {
                 $tstamp = time();
                 if ($otstamp && !$P0count) {
-                    $dt = $tstamp - $otstamp;
+                    $DT = $tstamp - $otstamp;
                 } else {
-                    $dt = 0;
+                    $DT = 0;
                 }
                 $otstamp = $tstamp;
                 $P0count = 0;
-                if($dt && $P) {
-                    $Etotal = $Etotal + $P*$dt*0.000000278;
+                if($DT && $P) {
+                    $Etotal = $Etotal + $P*$DT*0.000000278;
                 }
-                #file_put_contents("/var/www/html/dt.txt", $SDTE." Pnow=".$Pnow." P=".$P." dt=".$dt." E=".$Etotal."\r\n", FILE_APPEND);
+                #file_put_contents("/var/www/html/dt.txt", $SDTE." Pnow=".$Pnow." P=".$P." dt=".$DT." E=".$Etotal."\r\n", FILE_APPEND);
             } else {
                 $P0count++;
-                #file_put_contents("/var/www/html/dt.txt", $SDTE."### P=".$P." dt=".$dt." E=".$Etotal."\r\n", FILE_APPEND);
+                #file_put_contents("/var/www/html/dt.txt", $SDTE."### P=".$P." dt=".$DT." E=".$Etotal."\r\n", FILE_APPEND);
             }
         }
     } else {
-        $err="parse error";
+        $ERR="parse error";
     }
 }
-curl_close($ch);
+curl_close($CURL_HANDLE);
 sleep (5);
-file_put_contents("/var/www/html/dt.txt", $SDTE.": Pnow=".$Pnow." P=".$P." dt=".$dt." E=".$Etotal." Err=".$err."\r\n", FILE_APPEND);
+file_put_contents("/var/www/html/dt.txt", $SDTE.": Pnow=".$Pnow." P=".$P." dt=".$DT." E=".$Etotal." Err=".$ERR."\r\n", FILE_APPEND);
 if(!$P0count) $P=$Pnow; # check if valid actual value else keep last
 if($P0count>12) {
-    $P=(float) 0; # multiple times connection to mi600 failed set P=0
-    $dt=0;
+    $P=(float) 0; # multiple times ~60s connection to mi600 failed set P=0
+    $DT=0;
 }
 
 
-$INVT= 30; // Temperature inverter
-$BOOT = 30; // Temperature dc/dc booster
+
 
 // strings
 $I1V  = null;
@@ -99,15 +100,22 @@ $G3V = null;
 $G3A = null;
 $G3P = null;
 
-$I1V = (float) 34; // udc
+$INVT= 0; // Temperature inverter fixed dummy
+$BOOT = 0; // Temperature dc/dc booster fixed dummy
+$I1V = (float) 36; // udc fixed dummy
 $I1P = (float) $P;
 $I1A = (float) round($I1P/$I1V,2);
 $G1P= (float) $P; //pac
-$G1V= (float) 230; // uac
+$G1V= (float) 230; // uac fixed dummy
 $G1A = (float) round($G1P/$G1V,2);
-$FRQ= (float) 50;
+$FRQ= (float) 50; //freq  fixed dummy
 $KWHT= (float) $Etotal;
-$RET = 'OK';
+if($ERR) {
+    $RET = 'NOK';
+} else {
+    $RET = 'OK';
+}
+
 
 
 ?>
