60fc8178dbc3c51c9fbe1d95dc5cb0c8edcd56b7
soe135 <110527577+soe135@users.noreply.github.com>
2022-08-03 14:19:43 +0200

    Update mi600-webif.php
    Some changes that the protocol for mi-600 can operate with more than one mi-600 inverter in the same grid. Other changes solve the problem that the inverters webif not always deliver valid data.

--- scripts/protocols/mi600-webif.php
+++ scripts/protocols/mi600-webif.php
@@ -1,104 +1,44 @@
 <?php
-// Experimental ! haven't been tested
-
-# Stand-alone protocol for Bosswerk MI600 inverter
-# Author: https://github.com/dr-ni/123solar_mi600
+# Stand-alone protocol for Bosswerk MI600 (Deye SUN600G3-EU-230) inverter
+# Is now also working with more than one inverter in a grid.
+# Author: https://github.com/dr-ni/123solar_mi600, adapted by SOE135
 # License GPL-v3+
 #
 # Please supply '<host> <user> <passwd>' for your MI600 in Admin -> Inverter(s) configuration
 # Use the field 'Communication options'
 
 if (!defined('checkaccess')) {die('Direct access not permitted');}
-$LOGFILE = "/var/www/html/123solar/data/invt1/mi600.log";
-$LASTTOTALE = "/var/www/html/123solar/data/invt1/lastEtot.dat";
-$CMD_RETURN = ''; // Always initialize
+$CMD_RETURN = ''; // always initialize
 $MATCHES = '';
-if (!isset($P0count)) $P0count = 0;
-if (!isset($P)) $P = 0;
-if (!isset($DT)) $DT = 0;
-if (!isset($Etotal)) $Etotal = '';
-if (!isset($otstamp)) $otstamp= '';
 $ERR = "0";
 $SDTE = date("Ymd H:i:s");
 $OPTIONS = ${'COMOPTION'.$invt_num};
 list ($HOST, $USER, $PASSWD) = explode(" ", $OPTIONS, 3);
 $URL = "http://".$HOST."/status.html";
-
-// connect to mi600
-$connected = @fsockopen($HOST, 80);
-if ($connected){
-    fclose($connected);
-    // Decoding data
-    $PnowString = exec("curl -s -u ".$USER.":".$PASSWD ." ".$HOST."/status.html | grep \"webdata_now_p = \" | awk -F '\"' '{print $2}'");
-    if ($PnowString) {
-        $Pnow = (float) $PnowString;
-        $tstamp = time();
-        $DT = $tstamp - $otstamp;
-        $otstamp = $tstamp;
-        $P0count = 0;
-        if ($DEBUG) file_put_contents("$LOGFILE", $SDTE.": Pnow=".$Pnow." P=".$P." E=".$Etotal." Err=".$ERR."\r\n", FILE_APPEND);
-    } else {
-        $ERR = "could not read webdata_now_p";
-        $Pnow = (float) 0;
-        $P0count++;
-    }
-    if (!$Etotal) {
-        sleep(5);
-        $EtotalString = exec("curl -s -u ".$USER.":".$PASSWD ." ".$HOST."/status.html | grep \"webdata_total_e = \" | awk -F '\"' '{print $2}'");
-        $EtotalLast = (float) exec("cat ".$LASTTOTALE);
-        $Etotal = (float) $EtotalLast;
-        if ($EtotalString) {
-            if ((float) $EtotalString > $EtotalLast) $Etotal = (float) $EtotalString; # avoid jumping Etotal 
-            if ($DEBUG) file_put_contents("$LOGFILE", $SDTE." init local Etotal=".$Etotal."\r\n",FILE_APPEND);
-            $DT = 0;
-        } else {
-            $ERR = "could not read webdata_total_e";
-            $Etotal = '';
-        }
-    }
-} else {
-    $ERR = "could not connect to ".$HOST;
-    $Pnow = (float) 0;
-    $P0count++;
-}
-sleep (10); # if too long the power meter won't be updateing 
-if($DT && $P && ($P0count == 0) ) {
-    $dE=$P*$DT*0.000000278;
-    $Etotal = $Etotal + $dE;
-    if ($DEBUG) file_put_contents("$LOGFILE", "                   dE=".$dE." dt=".$DT."\r\n",FILE_APPEND);
-}
-file_put_contents("$LASTTOTALE", $Etotal);
-if($P0count==0) $P=(float) $Pnow; # check if valid actual value else keep last
-if($P0count>5) {
-    $P=(float) 0; # multiple times ~60s connection to mi600 failed set P=0
-    $tstamp = time();
-    $DT = $tstamp - $otstamp;
-    $otstamp = $tstamp;
-}
-
-
+$SRVDIR = $_SERVER['DOCUMENT_ROOT'];
+$LOGFILE = "$SRVDIR/123solar/data/invt".$invt_num."/mi600.log";
+$MI600_DATAFILE = "$SRVDIR/123solar/data/invt".$invt_num."/mi600.dat";
+$LAST_KWHTOTAL_FILE = "$SRVDIR/123solar/data/invt".$invt_num."/lastKWHtotal.dat";
 // strings
-$I1V  = null;
-$I1A  = null;
-$I1P  = null;
-$I2V  = null;
-$I2A  = null;
-$I2P  = null;
+$I1V = null;
+$I1A = null;
+$I1P = null;
+$I2V = null;
+$I2A = null;
+$I2P = null;
 $I3V = null;
 $I3A = null;
 $I3P = null;
 $I4V = null;
 $I4A = null;
 $I4P = null;
-
-// inverter
-$FRQ  = null;
-$EFF  = null;
+// inverters
+$FRQ = null;
+$EFF = null;
 $INVT = null;
 $BOOT = null;
 $KWHT = null;
-
-// grid
+// grids
 $G1V = null;
 $G1A = null;
 $G1P = null;
@@ -108,23 +48,116 @@ $G2P = null;
 $G3V = null;
 $G3A = null;
 $G3P = null;
+// variable names to store process values for the specific inverter
+$LastP = 'MI600INVT'.$invt_num.'_LASTP';
+$LastPTS = 'MI600INVT'.$invt_num.'_LASTPTS';
+$LastKWHT = 'MI600INVT'.$invt_num.'_LASTKWHT';
+// other needed variables
+$MaxRetryCount = 5;
+$RetryCounter = 0;
+$DataOK = 0;
+$P = (float) 0;
+$Dt = 0;
+$MinSecondsBetweenMeasurements = 10;
+$Now = time();
 
-$INVT= 0; // Temperature inverter fixed dummy
-$BOOT = 0; // Temperature dc/dc booster fixed dummy
-$I1V = (float) 36; // udc fixed dummy
-$I1P = (float) $P;
-$I1A = (float) round($I1P/$I1V,2);
-$G1P= (float) $P; //pac
-$G1V= (float) 230; // uac fixed dummy
-$G1A = (float) round($G1P/$G1V,2);
-$FRQ= (float) 50; //freq  fixed dummy
-$KWHT= (float) $Etotal;
-if($ERR != "0") {
-    $RET = 'OK'; #no NOK to enable automatic retry
-} else {
-    $RET = 'OK';
+// initializing process variables
+if (!isset($P)) $P = 0;
+if (!isset($$LastP)) $$LastP = (float) 0; // last determined power value for this inverter
+if (!isset($$LastPTS)) $$LastPTS = $Now - $MinSecondsBetweenMeasurements; // last determination time for the power value of this inverter
+if (!isset($$LastKWHT)) $$LastKWHT = (float) 0;  // last KWH-total value of this inverter
+
+// the first measurement should be taken immediately, the following ones at the earliest after $MinSecondsBetweenMeasurements seconds
+$SecondsElapsed = ($Now - $$LastPTS);
+if ($SecondsElapsed < $MinSecondsBetweenMeasurements) {
+  // we have to wait at least n sconds
+  sleep($MinSecondsBetweenMeasurements - $SecondsElapsed);
 }
 
+// connect to mi600/deye
+$Connected = @fsockopen($HOST, 80);
+if ($Connected) {
+  fclose($Connected);
+  // get data from webif
+  while (($RetryCounter < $MaxRetryCount) && ($DataOK == 0)) {
+    $Res = exec("curl -s -u ".$USER.":".$PASSWD ." ".$URL." > ".$MI600_DATAFILE);
+    // decoding the data
+    $PNowString = exec("grep \"webdata_now_p = \"  ".$MI600_DATAFILE."| awk -F '\"' '{print $2}'");
+    if ($PNowString) {
+      // The webif of the inverter could be read successfully.
+      $P = (float) $PNowString;
+      $$LastP = $P;
+      $DataOK = 1;
+      $Now = time();
+      $Dt = $Now - $$LastPTS;
+      $$LastPTS = $Now;
+      if (!$$LastKWHT) {
+        // If value for webdata_now_p could be read from webif it is sure that the value for webdata_total_e is also existing
+        $TotalKWHString = exec("grep \"webdata_total_e = \"  ".$MI600_DATAFILE."| awk -F '\"' '{print $2}'");
+        if ($TotalKWHString) {
+           $$LastKWHT = (float) $TotalKWHString;
+        }  
+        // check if our own calculated KWHT-value is higher then the value we read from inverters webif
+        // in this case we use our own stored value to avoid jumping KWHT backwards due to the missing decimal places 
+        // of the value from the webif
+        $StoredTotalKWH = (float) exec("cat ".$LAST_KWHTOTAL_FILE);
+        if ($StoredTotalKWH > $$LastKWHT) {
+          $$LastKWHT = $StoredTotalKWH;
+        }
+      }
+    } else {
+      // sometimes the values are not filled correctly by the webif, imho a bug in the webif of the inverter
+      $RetryCounter++;
+      if ($RetryCounter < $MaxRetryCount) {
+        // wait a second and try again
+        sleep (1);
+      } else {
+        // the inverter is alive but the webif is currently not delivering the correct values, so we use the last known power value
+        $P = $$LastP;
+        $Now = time();
+        $Dt = $Now - $$LastPTS;
+        $$LastPTS = $Now;
+      }
+    }
+  }  
+  if ($Dt && $P) {
+    // total-KWH value of this inverter has to be updated
+    $$LastKWHT += ($P * $Dt * ((1.0 / (60.0 * 60.0)) / 1000.0));
+    // save the value in case we stop the webif and restart it again later. 
+    file_put_contents("$LAST_KWHTOTAL_FILE", $$LastKWHT);    
+  } 
+} else {
+  $ERR = "could not connect to ".$HOST;
+  $$LastP = (float) 0;
+  $$LastPTS = time();
+}
 
+if ($DEBUG) {
+  if ($RetryCounter > 0) {
+    if ($RetryCounter < $MaxRetryCount) {
+       file_put_contents("$LOGFILE", $SDTE.": ".$RetryCounter." retries needed to read the data from webif!\r\n", FILE_APPEND);
+    } else {
+       file_put_contents("$LOGFILE", $SDTE.": After ".$RetryCounter." retries data could not be read from webif, so last known power value is used!\r\n", FILE_APPEND);
+    }  
+  }
+  file_put_contents("$LOGFILE", $SDTE.": P=".$P." P_LAST=".$$LastP." KWHTotal=".$$LastKWHT." Err=".$ERR."\r\n", FILE_APPEND);
+}  
+
+$INVT= 0; // temperature inverter fixed dummy
+$BOOT = 0; // temperature dc/dc booster fixed dummy
+$I1V = (float) 36; // udc fixed dummy
+$I1P = (float) $P;
+$I1A = (float) round($I1P / $I1V, 2);
+$G1P= (float) $P; // P-AC
+$G1V= (float) 230; // U-AC fixed dummy
+$G1A = (float) round($G1P / $G1V, 2);
+$FRQ= (float) 50; // freq  fixed dummy
+$KWHT = $$LastKWHT; 
+
+if ($ERR == "0") {
+  $RET = 'OK';
+} else {
+  $RET = 'NOK';
+}
 
 ?>
