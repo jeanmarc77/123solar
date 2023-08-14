278126cb9818e45cdc21cc828660a633380a5263
nethawk2 <52479299+nethawk2@users.noreply.github.com>
2023-08-14 12:00:07 +0200

    Support solaredge-modbus-tcp
    This little helper builds the bridge between 123solar and Solaredge Inverters with activated modbus-TCP with the help of sunspec-monitor https://github.com/tjko/sunspec-monitor/ so it is more a protocol-helper than it does implement a protocol itself

---
+++ scripts/protocols/solaredge-modbustcp_checks.php
@@ -0,0 +1,18 @@
+<?php
+if (!defined('checkaccess')) {die('Direct access not permitted');}
+
+// State
+$STATE = 'no invt status';
+
+// Alarms
+$ALARM = null;
+$MESSAGE = null;
+
+// Riso, iLeak - Peak Powers
+$RISO = 0;
+$ILEAK = 0;
+$PPEAK = 0;
+$PPEAKOTD = 0;
+$RET = 'NOK'; // No output
+
+?>
