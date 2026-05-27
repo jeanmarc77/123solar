5e6dfe159c9189ba504a4dece3a123556707e51a
Jean-Marc Louviaux <jeanmarc.louviaux@gmail.com>
2026-05-27 14:58:13 +0200

    Update sma_pool.php

--- misc/sma-modbus/sma_pool.php
+++ misc/sma-modbus/sma_pool.php
@@ -8,74 +8,69 @@ if (isset($_SERVER['REMOTE_ADDR'])) {
 
 $SMAIP = '192.168.50.127';
 $PYTHON_VENV = "/srv/http/comapps/sma-modbus/py-sma-modbus2/run/bin/python";
-$SCRIPT_PATH = "/srv/http/comapps/sma-modbus/py-sma-modbus2/main.py";
+$SCRIPT_PATH = "/srv/http/comapps/sma-modbus/py-sma-modbus2/main.py 2>/dev/null";
 $modbusregisters = "/srv/http/comapps/sma-modbus/sma-modbusregisters.txt";
 
-
 if (!isset($argv[1])) {
-	die("No command\n");
+    die("No command\n");
 }
 
-if ($argv[1] == '-live') {
-$poolcmd = "$PYTHON_VENV $SCRIPT_PATH -a " . escapeshellarg($SMAIP) . " -log -f $modbusregisters";
-//$poolcmd = "$PYTHON_VENV $SCRIPT_PATH -a " . escapeshellarg($SMAIP) . " -f $modbusregisters";
-} elseif ($argv[1] == '-alarm') {
-$poolcmd = "$PYTHON_VENV $SCRIPT_PATH -a " . escapeshellarg($SMAIP) . " -log 30211";
-} elseif ($argv[1] == '-message') {
-$poolcmd = "$PYTHON_VENV $SCRIPT_PATH -a " . escapeshellarg($SMAIP) . " -log 30213";
-} elseif ($argv[1] == '-state') {
-$poolcmd = "$PYTHON_VENV $SCRIPT_PATH -a " . escapeshellarg($SMAIP) . " -log 30225";
-} elseif ($argv[1] == '-ileak') {
-$poolcmd = "$PYTHON_VENV $SCRIPT_PATH -a " . escapeshellarg($SMAIP) . " -log 31247";
-} elseif ($argv[1] == '-riso') {
-$poolcmd = "$PYTHON_VENV $SCRIPT_PATH -a " . escapeshellarg($SMAIP) . " -log 30225";
+$action = $argv[1];
+
+if ($action == '-info') {
+    $poolarr = [];
+    $poolret = 0;
+    $poolcmd = "$PYTHON_VENV $SCRIPT_PATH -a " . escapeshellarg($SMAIP) . " -log 30057";
+    exec($poolcmd, $poolarr, $poolret);
+    
+    if ($poolret === 0 && !empty($poolarr)) {
+        echo "serial " . $poolarr[0];
+    } else {
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode(["error" => "-info", "code" => $poolret]);
+    }
+    exit;
 }
+
+$poolcmd = '';
+if ($action == '-live') {
+    $poolcmd = "$PYTHON_VENV $SCRIPT_PATH -a " . escapeshellarg($SMAIP) . " -log -f $modbusregisters";
+} elseif ($action == '-alarm') {
+    $poolcmd = "$PYTHON_VENV $SCRIPT_PATH -a " . escapeshellarg($SMAIP) . " -log 30211";
+} elseif ($action == '-message') {
+    $poolcmd = "$PYTHON_VENV $SCRIPT_PATH -a " . escapeshellarg($SMAIP) . " -log 30213";
+} elseif ($action == '-state') {
+    $poolcmd = "$PYTHON_VENV $SCRIPT_PATH -a " . escapeshellarg($SMAIP) . " -log 30225";
+} elseif ($action == '-ileak') {
+    $poolcmd = "$PYTHON_VENV $SCRIPT_PATH -a " . escapeshellarg($SMAIP) . " -log 31247";
+} elseif ($action == '-riso') {
+    $poolcmd = "$PYTHON_VENV $SCRIPT_PATH -a " . escapeshellarg($SMAIP) . " -log 30225";
+} else {
+    die("Unknown command: $action\n");
+}
+
 $poolarr = [];
 $poolret = 0;
-if ($argv[1] != '-info') {
 exec($poolcmd, $poolarr, $poolret);
-}
-//print_r($poolarr);
-if ($poolret === 0 && $argv[1] == '-live') {
-	header('Content-Type: application/json; charset=utf-8');
-	echo json_encode($poolarr);
-	exit;
-} elseif ($poolret === 0 && $argv[1] == '-alarm') {
-	if ($poolarr[0] !='keine (NonePrio)') {
-	echo $poolarr[0];
-	} else {
-	echo '';
-	}
-} elseif ($poolret === 0 && $argv[1] == '-message') {
-	if ($poolarr[0] !='keine (NoneMsg)') {
-	echo $poolarr[0];
-	} else {
-	echo '';
-	}
-} elseif ($poolret === 0 && $argv[1] == '-state') {
-	if ($poolarr[0] !='skeine (NoneMsg)') {
-	echo $poolarr[0];
-	} else {
-	echo '';
-	}
-} elseif ($poolret === 0 && $argv[1] == '-ileak') {
-	echo $poolarr[0];
-} elseif ($poolret === 0 && $argv[1] == '-riso') {
-	echo $poolarr[0];
-} elseif ($argv[1] == '-info') {
-	$poolarr = [];
-	$poolret = 0;
-	$ret = '';
-	$poolcmd = "$PYTHON_VENV $SCRIPT_PATH -a " . escapeshellarg($SMAIP) . " -log 30057";
-	exec($poolcmd, $poolarr, $poolret);
-	if ($poolret === 0) {
-	$ret = "serial $poolarr[0]";
-	}
-	echo $ret;
-	
-} else {
-    echo "Error " . $poolret;
-}
-   
 
+if ($poolret === 0 && !empty($poolarr)) {
+    if ($action == '-live') {
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode($poolarr);
+        exit;
+    } elseif ($action == '-alarm') {
+        echo ($poolarr[0] != 'keine (NonePrio)') ? $poolarr[0] : '';
+    } elseif ($action == '-message') {
+        echo ($poolarr[0] != 'keine (NoneMsg)') ? $poolarr[0] : '';
+    } elseif ($action == '-state') {
+        echo ($poolarr[0] != 'skeine (NoneMsg)') ? $poolarr[0] : '';
+    } elseif ($action == '-ileak' || $action == '-riso') {
+        echo $poolarr[0];
+    }
+} else {
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode(["error" => "py", "code" => $poolret]);
+    exit;
+}
 ?>
+
