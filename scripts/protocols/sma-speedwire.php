b3e3aac8c5048b584f66e26677b9114fd132010a
Jean-Marc Louviaux <jeanmarc.louviaux@gmail.com>
2026-05-29 06:07:53 +0200

    Update sma-speedwire.php

--- scripts/protocols/sma-speedwire.php
+++ scripts/protocols/sma-speedwire.php
@@ -62,7 +62,7 @@ if ($CMD_RETURN === 0 && isset($dataarray[13])) {
 		$G3A = null;
 		$G3P = null;
 		if ($I1P > 0 && $G1P > 0) {
-		$EFF  = ($G1P/$I1P)*100;
+		$EFF  = ($G1P/($I1P+$I2P))*100;
 		} else {
 		$EFF = 0;
 		}
